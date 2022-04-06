7970a0a98e2ef7c30a54194827fb1304117ad1d2
kiky <kikyfadhilah2001@gmail.com>
2022-04-06 21:13:53 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/id/informatika/a160419134_kiky_uts/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.id.informatika.a160419134_kiky_uts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
